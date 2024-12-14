7f3ef7ed8d4912d48fff0899bec1ccfafe6040eb
olegg <s.gelo2004@gmail.com>
2024-12-14 19:32:13 +0300

    feat: add role creating validator

---
+++ IdentityService/src/MediaReview.Identity.Application/Identity/Commands/Role/CreateRoleCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+
+namespace MediaReview.Identity.Application.Identity.Commands.Role;
+
+public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+{
+    public CreateRoleCommandValidator(RoleManager<Domain.Entities.Role> roleManager)
+    {
+        RuleFor(command => command.Name)
+            .NotEmpty()
+            .WithMessage("Role name is required.")
+            .MaximumLength(20)
+            .WithMessage("Role name cannot exceed 20 characters.")
+            .MustAsync(async (name, cancellationToken) =>
+            {
+                var existingRole = await roleManager.FindByNameAsync(name);
+                return existingRole == null;
+            })
+            .WithMessage("A role with the given name already exists.");
+
+        RuleFor(command => command.IsActive)
+            .Must(value => value is bool)
+            .WithMessage("IsActive must be a boolean value.");
+    }
+}
