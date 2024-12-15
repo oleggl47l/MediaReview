d351bf7924326820566381ad178403e3ae873eb1
olegg <s.gelo2004@gmail.com>
2024-12-15 22:26:02 +0300

    chore: simplify entity

--- ReviewService/src/MediaReview.Review.Domain/Entities/Review.cs
+++ ReviewService/src/MediaReview.Review.Domain/Entities/Review.cs
@@ -5,11 +5,11 @@ public class Review
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
-    public Guid AuthorId { get; private set; }
+    public Guid AuthorId { get; set; }
     public Guid CategoryId { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    public bool IsPublished { get; private set; } = false;
+    public bool IsPublished { get; set; } = true;
     
     public Category? Category { get; set; }
     public ICollection<ReviewTag> ReviewTags { get; set; } = new List<ReviewTag>();
